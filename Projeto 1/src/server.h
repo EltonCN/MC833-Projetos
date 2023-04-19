f14215f8ed79a7e2f77911484cb7c6564b95709b
Leonardo Rener <leonardo-rener@hotmail.com>
2023-04-19 09:23:02 -0300

    Fix server implementation

--- Projeto 1/src/server.h
+++ Projeto 1/src/server.h
@@ -5,12 +5,12 @@
 #define SERVER
 
 Response* REGISTER_handler(Request request);
-Response REMOVER_handler(char where_cause[70]);
+Response* REMOVER_handler(char where_cause[70]);
 Response* LIST_USER_handler(char where_cause[70]);
 Response* LIST_BY_COURSE_handler(Request request);
 Response* LIST_BY_SKILL_handler(Request request);
 Response* LIST_BY_YEAR_handler(Request request);
-Response GET_BY_MAIL_handler(Request request);
-Response REMOVE_BY_MAIL_handler(Request request);
+Response* GET_BY_MAIL_handler(Request request);
+Response* REMOVE_BY_MAIL_handler(Request request);
 
 #endif

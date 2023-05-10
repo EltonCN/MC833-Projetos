081f15b129791e9cdf38f361a9cad4656fb37a57
Leonardo Rener <leonardo-rener@hotmail.com>
2023-05-10 09:56:42 -0300

    Added serverUDP

--- Projetos 1 e 2/build.sh
+++ Projetos 1 e 2/build.sh
@@ -1,2 +1,4 @@
 gcc -o build/client -w -O3 src/client.c src/client_tcp.c 
 gcc -o build/server $(mysql_config --cflags) src/server_tcp.c src/server.c $(mysql_config --libs) -D _GNU_SOURCE
+
+gcc -o build/serverUDP $(mysql_config --cflags) src/server_udp.c src/server.c $(mysql_config --libs) -D _GNU_SOURCE
